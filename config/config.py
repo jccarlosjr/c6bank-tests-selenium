41737c9e43bcd36dc88199cc9e63c6789b39b2ca
Carlos Junior <96848712+jccarlosjr@users.noreply.github.com>
2025-06-16 11:25:34 -0300

    Initial commit: setup Selenium automation for C6 Bank

--- config/config.py
+++ config/config.py
@@ -2,6 +2,6 @@ CHROMEDRIVER_PATH = ".//drivers//webdrivers//chromedriver.exe"
 
 LOCAL_USER_PATH = r"--user-data-dir=C:\Users\Junior\AppData\Local\Google\Chrome\User Data\Profile 1" 
 
-USER = "01862439524_002835"
+USER = " "
 
-PASSWORD = "Chesjr@41"
+PASSWORD = " "
